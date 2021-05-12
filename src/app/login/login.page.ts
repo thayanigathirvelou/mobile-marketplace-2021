7fd171c969792d6ca9e3a6d172609c8018fe7bf3
webtegy <49366481+webtegy@users.noreply.github.com>
2021-05-12 02:27:29 -0400

    login and register page completed

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  email = ''; 
+  password = '';
+  errorMessage='';
+  constructor(private router:Router, private authService:AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  onLogin(form:NgForm){
+    if(form.valid){
+     //call authentication service method to login
+     this.authService.SignIn(this.email, this.password).then((results)=>{
+       console.log(results);
+       if(results.user && results.user.emailVerified){
+         console.log(results.user);
+         this.router.navigate(['']);
+       }
+       else{
+         this.router.navigateByUrl('/register');
+         return false; 
+       }
+
+     }).catch((error)=>{
+       if(error.code == 'auth/user-not-found'){
+         this.errorMessage = 'There is no user record corresponding to this email. Please try again'
+       }
+       else{
+        this.errorMessage = error.message; 
+      }
+     })
+    }
+
+  }
+
+  register(){
+    //route to the registration page.
+    this.router.navigate(['register']); 
   }
 
 }
